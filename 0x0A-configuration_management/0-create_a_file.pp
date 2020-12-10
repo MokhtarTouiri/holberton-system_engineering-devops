f87b2813ea4d0f5852ab8f15f138efd0bb7b68e3
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-10 20:33:35 +0000

    Created by Mokhtar Tiouiri

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,7 +4,5 @@ file { '/tmp/holberton':
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  content  => "I love Puppet",
-   }
+  content  => 'I love Puppet',
 }
-[root@UA-HA manifests]#
