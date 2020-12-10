c671d0532d0a270a591b0238dff7365166285e29
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-10 22:18:05 +0000

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Create a file in /tmp Using Puppet
+#Create a file
 file { '/tmp/holberton':
   ensure  => 'file',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  content  => 'I love Puppet',
+  content => 'I love Puppet',
 }
