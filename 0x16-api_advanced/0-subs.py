d4c897e62d59560b33fb01c344c35991880b6bc6
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-18 22:13:36 +0000

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,8 @@ def number_of_subscribers(subreddit):
     0. How many subs?
     """
     header = {"User-Agent": "My-User-Agent"}
-    res = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers=header, 
-                            allow_redirects=False)
+    res = requests.get("https://www.reddit.com/r/{}/about.json".format(
+        subreddit), headers=header, allow_redirects=False)
     if res.status_code >= 300:
         return 0
 
